851f9dc367a56add0555c324f46fae034be6e16b
Manantsoa <rtianamanantsoa@gmail.com>
2021-05-29 14:16:49 +0300

    initial commit

--- tests/test_hello_world.py
+++
@@ -1,7 +0,0 @@
-# Azure will require tests in order to deploy the app.
-# Feel free to add your own that actually test functionality.
-def add(a):
-    return a + 1
-
-def test_add_function():
-    assert add(1) == 2
